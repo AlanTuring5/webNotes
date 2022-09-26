1c4aa26ee5369b93779e536af6b2a26ca368e73a
alanturing <1563737131@qq.com>
2022-09-26 10:38:09 +0800

    vue3.0响应式原理

--- 前端猎人小野森森1/vue响应式原理/vue3.0响应式原理Proxy/0Proxy.js
+++ 前端猎人小野森森1/vue响应式原理/vue3.0响应式原理Proxy/0Proxy.js
@@ -2,3 +2,21 @@
 
 // Proxy ES6 构造函数
 
+const target = {
+    a: 1,
+    b: 2,
+}
+
+const proxy = new Proxy(target, {
+    get(target, prop) {
+        return '"this target\'s value is"' + target[prop];
+    },
+    set(target, prop, value) {
+        target[prop] = value;
+        console.log(target[prop]);
+    }
+});
+
+console.log(proxy.a);
+console.log(target.a);
+proxy.b = 3;
