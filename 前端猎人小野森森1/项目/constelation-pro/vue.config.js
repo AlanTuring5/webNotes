09c2345a78efba583130ab696f2b8bc54781d981
alanturing <1563737131@qq.com>
2022-10-10 15:22:59 +0800

    跨域配置

--- 前端猎人小野森森1/项目/constelation-pro/vue.config.js
+++ 前端猎人小野森森1/项目/constelation-pro/vue.config.js
@@ -1,4 +1,29 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 跨域
+  devServer: {
+    proxy: {
+      '/api': {
+        // 目标源
+        target: 'http://web.juhe.cn:8080',
+        // 换源
+        changeOrigin: true,
+        // websocket
+        ws: true,
+        // 不检查https
+        secure: false,
+        // 目录重写
+        pathRewrite: {
+          '^api': ''
+        }
+      }
+    },
+    overlay: {
+      warning: false,
+      errors: false,
+    }
+  },
+  // 设置是否在开发环境下每次保存代码时都启用eslint验证
+  lintOnSave: false,
 })
